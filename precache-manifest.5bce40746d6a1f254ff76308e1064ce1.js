46b118424d84a3669f89a23dc3663a80dd2486b9
shuaigechen <707626760@qq.com>
2020-09-09 18:44:41 +0800

    deploy

---
+++ precache-manifest.5bce40746d6a1f254ff76308e1064ce1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dba69a15d9ceec512832",
+    "url": "/afeng2.0/css/app.16c64e5c.css"
+  },
+  {
+    "revision": "9577bdc78dced049d9ac2bfc46bf05f3",
+    "url": "/afeng2.0/index.html"
+  },
+  {
+    "revision": "dba69a15d9ceec512832",
+    "url": "/afeng2.0/js/app.5e350b04.js"
+  },
+  {
+    "revision": "4ea88a06bd5f3f94401d",
+    "url": "/afeng2.0/js/chunk-vendors.f79321a9.js"
+  },
+  {
+    "revision": "b31d619fbb2d6efc348eb5ac2f8736f4",
+    "url": "/afeng2.0/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/afeng2.0/robots.txt"
+  }
+]);
